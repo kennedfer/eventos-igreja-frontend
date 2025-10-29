2f2a5af8fb169a3221eeb9757f3bfb1772da0778
kennedfer <kenned14santos@gmail.com>
2025-10-29 18:59:56 -0300

    fix: fix api_url in creation page

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -4,53 +4,58 @@ import { EventForm } from "../components/EventForm";
 import toast from "react-hot-toast";
 import { useState } from "react";
 import { useEvents } from "../contexts/EventsContext";
+import { API_URL } from "../utils/env";
 
 export function Create() {
-    const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const {mutate} = useEvents();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { mutate } = useEvents();
 
-    async function handleSubmit(data: Partial<ChurchEvent>) {
-        setIsLoading(true);
-        try {
-            const response = await fetch("http://localhost:8080/api/v1/events", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(data),
-                credentials:"include"
-            });
+  async function handleSubmit(data: Partial<ChurchEvent>) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(API_URL + "/api/v1/events", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+        credentials: "include",
+      });
 
-            if (response.status === 401) {
-                toast.error("Você deve estar logado para criar um novo evento");
-                return navigate("/entrar");
-            }
-    
-            const { success} = await response.json();
-    
-            if (success) {
-                await mutate();
-                toast.success("Evento criado com sucesso");
-                navigate("/eventos");
-            }else{
-                toast.error("Erro ao criar evento");
-            }
-    
-        } catch (err: any) {
-            toast.error(err.message.includes('Failed to fetch')
-                ? 'Erro de conexão com o servidor, tente novamente mais tarde'
-                : 'Ocorreu um erro inesperado');
-        } finally {
-            setIsLoading(false);
-        }
+      if (response.status === 401) {
+        toast.error("Você deve estar logado para criar um novo evento");
+        return navigate("/entrar");
+      }
+
+      const { success } = await response.json();
+
+      if (success) {
+        await mutate();
+        toast.success("Evento criado com sucesso");
+        navigate("/eventos");
+      } else {
+        toast.error("Erro ao criar evento");
+      }
+    } catch (err: any) {
+      toast.error(
+        err.message.includes("Failed to fetch")
+          ? "Erro de conexão com o servidor, tente novamente mais tarde"
+          : "Ocorreu um erro inesperado"
+      );
+    } finally {
+      setIsLoading(false);
     }
-    
+  }
 
-    return (
-        <div className="w-full h-auto items-center flex flex-col">
-            <div className="p-5 w-full md:w-[600px] mb-10">
-                <h1 className="mb-3 mt-6 text-3xl font-bold">Criar novo evento</h1>
-                <EventForm isLoading={isLoading} onCancel={() => navigate(-1)} onSubmit={handleSubmit} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full h-auto items-center flex flex-col">
+      <div className="p-5 w-full md:w-[600px] mb-10">
+        <h1 className="mb-3 mt-6 text-3xl font-bold">Criar novo evento</h1>
+        <EventForm
+          isLoading={isLoading}
+          onCancel={() => navigate(-1)}
+          onSubmit={handleSubmit}
+        />
+      </div>
+    </div>
+  );
 }
